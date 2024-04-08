a4e09597ad127540272fef05f81a117ff524954a
Francis <fhrancorey99@gmail.com>
2024-04-08 11:29:14 +0100

    fix: allowed hosts

--- covers_and_spines/settings.py
+++ covers_and_spines/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 ENVIRONMENT = env.str('ENVIRONMENT', default='production')
 
