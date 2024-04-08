e32589cc93545865ef88cc8f0026b0242610f95f
Francis <fhrancorey99@gmail.com>
2024-04-08 14:57:20 +0100

    fix: db on settings.py

--- covers_and_spines/settings.py
+++ covers_and_spines/settings.py
@@ -112,16 +112,17 @@ WSGI_APPLICATION = 'covers_and_spines.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
-        'HOST': env.str('DB_HOST'),
-        'PORT': env.str('DB_PORT'),
-    }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env.str('DB_NAME'),
+#         'USER': env.str('DB_USER'),
+#         'PASSWORD': env.str('DB_PASSWORD'),
+#         'HOST': env.str('DB_HOST'),
+#         'PORT': env.str('DB_PORT'),
+#     }
+ }
 
 
 # Password validation
