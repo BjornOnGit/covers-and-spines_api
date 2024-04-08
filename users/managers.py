578aea4213124a79d87459c686861634cc9195b8
Francis <fhrancorey99@gmail.com>
2024-04-08 15:24:43 +0100

    fix: superuser method in managers.py

--- users/managers.py
+++ users/managers.py
@@ -20,6 +20,17 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(username, email, password, **extra_fields)
+    
     def create_password_reset_token(self, user):
         PasswordReset = apps.get_model('users', 'PasswordReset')
         token = get_random_string(length=32)
